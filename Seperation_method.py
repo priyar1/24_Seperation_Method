cec9d8abd2a6c44515d6a991bc2bd6bc721e07ae
priyar1 <122872326+priyar1@users.noreply.github.com>
2023-01-31 10:20:54 +0530

    Add files via upload

---
+++ Seperation_method.py
@@ -0,0 +1,9 @@
+# print('hello'   ,  'world')
+# print('hello' +' '+   'world')
+# print('hello'+chr(32)+'world')
+
+# print('python'+"-"+'program'+'-'+'world')
+
+# print('hello', 'world',sep="*")
+
+# print('p','y','t','h','o','n',sep='@')
